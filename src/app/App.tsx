cab05d37213669cb39b993a4d9244bda841a61fb
Marcin Bołba <marcin.bolba@brainhub.pl>
2023-09-18 23:23:08 +0200

    Add GlobalStyles

---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+import GlobalStyles from "./GlobalStyles/GlobalStyles";
+import Home from "@/pages/Home/Home";
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <Home />
+    </>
+  );
+}
+
+export default App;
---
+++ src/app/GlobalStyles/GlobalStyles.tsx
@@ -0,0 +1,47 @@
+import { Global, css } from "@emotion/react";
+
+// fonts
+import MontserratRegularTtf from "@/assets/fonts/Montserrat-Regular.ttf";
+import MontserratMediumTtf from "@/assets/fonts/Montserrat-Medium.ttf";
+
+const globalCss = css`
+  html {
+    box-sizing: border-box;
+  }
+
+  html,
+  body {
+    margin: 0;
+  }
+
+  *,
+  *:before,
+  *:after {
+    box-sizing: inherit;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  #root {
+    width: 100%;
+    height: 100vh;
+  }
+
+  @font-face {
+    font-family: "Montserrat";
+    src: url(${MontserratRegularTtf}) format("truetype");
+    font-weight: 400;
+  }
+
+  @font-face {
+    font-family: "Montserrat";
+    src: url(${MontserratMediumTtf}) format("woff2");
+    font-weight: 500;
+  }
+`;
+
+const GlobalStyles = () => <Global styles={globalCss} />;
+
+export default GlobalStyles;
