edd2b06a3f1e3dda09795786da3efee9f0e33b8b
Marcin Bołba <marcin.bolba@brainhub.pl>
2023-09-18 23:23:08 +0200

    Add GlobalStyles

---
+++ src/pages/Home/Home.styled.tsx
@@ -0,0 +1,7 @@
+import styled from "@emotion/styled";
+
+const HelloLabel = styled("span")`
+  color: red;
+`;
+
+export default { HelloLabel };
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,9 @@
+import S from "./Home.styled";
+
+const Home = () => {
+  const x = "hello home";
+
+  return <S.HelloLabel>{x}</S.HelloLabel>;
+};
+
+export default Home;
