16165547ae1fcc1d10fecec777308fd09bbe4cc6
Marcin Bołba <marcin.bolba@brainhub.pl>
2023-09-24 20:43:19 +0200

    Add icons

---
+++ src/assets/images/FacebookIcon.tsx
@@ -0,0 +1,30 @@
+const FacebookIcon = () => (
+  <svg
+    width="25"
+    height="25"
+    viewBox="0 0 14 25"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M8 9.45833H14L13.3333 12.125H8V24.125H5.33333V12.125H0V9.45833H5.33333V6.96233C5.33333 4.585 5.58133 3.72233 6.04533 2.853C6.50001 1.99408 7.20241 1.29168 8.06133 0.837C8.93067 0.373 9.79333 0.125 12.1707 0.125C12.8667 0.125 13.4773 0.191667 14 0.325V2.79167H12.1707C10.4053 2.79167 9.868 2.89567 9.31867 3.189C8.92436 3.39501 8.60277 3.71706 8.39733 4.11167C8.104 4.65967 8 5.197 8 6.96233V9.45833Z"
+      fill="url(#paint0_linear_219_1962)"
+    />
+    <defs>
+      <linearGradient
+        id="paint0_linear_219_1962"
+        x1="7"
+        y1="0.125"
+        x2="7"
+        y2="24.125"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stop-color="white" stop-opacity="0" />
+        <stop offset="0.0001" stop-color="white" />
+        <stop offset="1" stop-color="#F7FAFF" />
+      </linearGradient>
+    </defs>
+  </svg>
+);
+
+export default FacebookIcon;
