626a50db617378d24f89a61eaf1dea7d1d6660da
Marcin Bołba <marcin.bolba@brainhub.pl>
2023-09-25 08:43:48 +0200

    Add TeamViewWrapper

--- src/pages/Team/TeamView.styled.tsx
+++ src/pages/Team/TeamView.styled.tsx
@@ -1,12 +1,13 @@
 import styled from "@emotion/styled";
 
+const ViewWrapper = styled.div`
+  padding: 120px 80px;
+`;
+
 const SectionsWrapper = styled.div`
   display: flex;
   flex-wrap: wrap;
-  flex-direction: column;
   gap: 40px;
 `;
 
-export default {
-  SectionsWrapper,
-};
+export default { ViewWrapper, SectionsWrapper };
