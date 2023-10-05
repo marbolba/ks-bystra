ff7119719d4dc4713119dd762dee2f90204145d8
Marcin Bołba <marcin.bolba@brainhub.pl>
2023-10-05 19:07:45 +0200

    Add colors to footer labels

--- src/components/IconWithText/IconWithText.tsx
+++ src/components/IconWithText/IconWithText.tsx
@@ -4,12 +4,15 @@ import S from "./IconWithText.styled";
 type Props = {
   icon: React.ReactNode;
   text: string;
+  color: string;
 };
 
-const IconWithText = ({ icon, text }: Props) => (
+const IconWithText = ({ icon, text, color }: Props) => (
   <S.IconWithTextWrapper>
     {icon}
-    <Typography variant="body1">{text}</Typography>
+    <Typography variant="body1" color={color}>
+      {text}
+    </Typography>
   </S.IconWithTextWrapper>
 );
 
