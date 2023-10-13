69c0c315040ef65d7f56a2016150b55ceac86c01
Marcin Bołba <marcin.bolba@brainhub.pl>
2023-10-13 03:42:39 +0200

    Move routes away from app

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,69 +1,15 @@
 import GlobalStyles from "./GlobalStyles/GlobalStyles";
-import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
 import ThemeProvider from "@/theme";
 import { CssBaseline } from "@mui/material";
-import Section from "@/pages/Section/Section";
-import Header from "@/components/Header/Header";
-import Footer from "@/components/Footer/Footer";
-import Landing from "@/pages/Landing/Landing";
-import NavigationBar from "@/components/NavigationBar/NavigationBar";
-import { useCallback, useEffect, useState } from "react";
-import useJsonTemplates from "@/hooks/useJsonTemplates";
+import Routes from "./Routes";
 
 function App() {
-  const [sectionsList, setSectionsList] = useState<string[] | null>(null);
-  const { fetchSectionsList } = useJsonTemplates();
-
-  const initializeRouter = useCallback(async () => {
-    const sectionData = fetchSectionsList;
-    setSectionsList(sectionData);
-  }, []);
-
-  const getSectionsTemplate = useCallback(
-    (sectionsList: string[]) =>
-      sectionsList.map((sectionName: string, idx) => (
-        <Route
-          key={idx}
-          path={sectionName}
-          element={<Section id={sectionName} />}
-        />
-      )),
-    []
-  );
-
-  useEffect(() => {
-    initializeRouter();
-  });
-
-  if (!sectionsList) {
-    return null;
-  }
-
-  const sectionsTemplate = getSectionsTemplate(sectionsList);
-
-  // TODO Move away
-  const Layout = () => (
-    <>
-      <Header />
-      <NavigationBar />
-      <Outlet />
-      <Footer />
-    </>
-  );
-
   return (
     <>
       <GlobalStyles />
       <ThemeProvider>
         <CssBaseline>
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<Layout />}>
-                <Route path="/" element={<Landing />} />
-                {sectionsTemplate}
-              </Route>
-            </Routes>
-          </BrowserRouter>
+          <Routes />
         </CssBaseline>
       </ThemeProvider>
     </>
---
+++ src/app/Routes.tsx
@@ -0,0 +1,74 @@
+import Section from "@/pages/Section/Section";
+import Header from "@/components/Header/Header";
+import Footer from "@/components/Footer/Footer";
+import Landing from "@/pages/Landing/Landing";
+import NavigationBar from "@/components/NavigationBar/NavigationBar";
+import { useCallback, useEffect, useState } from "react";
+import useJsonTemplates from "@/hooks/useJsonTemplates";
+import {
+  Outlet,
+  Route,
+  Routes as ReactRoutes,
+  BrowserRouter,
+} from "react-router-dom";
+import { styled } from "@mui/material";
+
+const Routes = () => {
+  const [sectionsList, setSectionsList] = useState<string[] | null>(null);
+  const { fetchSectionsList } = useJsonTemplates();
+
+  const initializeRouter = useCallback(async () => {
+    const sectionData = fetchSectionsList;
+    setSectionsList(sectionData);
+  }, []);
+
+  const getSectionsTemplate = useCallback(
+    (sectionsList: string[]) =>
+      sectionsList.map((sectionName: string, idx) => (
+        <Route
+          key={idx}
+          path={sectionName}
+          element={<Section id={sectionName} />}
+        />
+      )),
+    []
+  );
+
+  useEffect(() => {
+    initializeRouter();
+  });
+
+  if (!sectionsList) {
+    return null;
+  }
+
+  const LayoutWrapper = styled("div")`
+    width: 100%;
+    height: 100%;
+    margin: 0 auto;
+    max-width: 1440px;
+  `;
+
+  const Layout = () => (
+    <LayoutWrapper>
+      <Header />
+      <NavigationBar />
+      <Outlet />
+      <Footer />
+    </LayoutWrapper>
+  );
+
+  const sectionsTemplate = getSectionsTemplate(sectionsList);
+  return (
+    <BrowserRouter>
+      <ReactRoutes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Landing />} />
+          {sectionsTemplate}
+        </Route>
+      </ReactRoutes>
+    </BrowserRouter>
+  );
+};
+
+export default Routes;
